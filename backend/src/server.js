58ac22751873740c89847572f8f1a1839aa245f1
Vladyslav Mishchenko <vlad.mishh@gmail.com>
2025-10-04 09:13:29 +0300

    added server startup logs

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,19 +16,30 @@ const PORT = ENV.EXPRESS_SERVER_PORT;
 
 const __dirname = path.resolve();
 
+console.log("server.js entry");
+
 // app.use(passport.initialize());
 
 app.use(express.json({limit: "6mb"})); //for parsing req.body, 6mb max file size
-app.use(cors({origin: ENV.CLIENT_URL, credentials: true}));  //Cross-Origin Resource Sharing? allows browser requesting resources from different domain, that loaded the page
-app.use(cookieParser()); //allows parsing cookies
+console.log("json parsing enabled..");
 
+app.use(cors({origin: ENV.CLIENT_URL, credentials: true}));  //Cross-Origin Resource Sharing? allows browser requesting resources from different domain, that loaded the page
+console.log("cors policy cofigured");
+
+app.use(cookieParser()); //allows parsing cookies
+console.log("cookie parser enabled");
 
 app.use("/api/messages", messageRoutes);
+console.log("message routes enabled");
+
 app.use("/api/auth", authRoutes);
+console.log("authentification routes enabled");
+
 
 
 //creates 3 quotes bots
 seedBots();
+console.log("bots seeded");
 
 //if in deploy - sends frontend page via express server endpoint
 if(ENV.NODE_ENV === "production"){
@@ -38,9 +49,11 @@ if(ENV.NODE_ENV === "production"){
     app.use((_,res) => { // use this this in express v5+
         res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"))
     })
+    console.log("production build redirect to the index.html static enabled");
 }
 
 server.listen(PORT, () => {
+    console.log("trying connecting to the db");
     connectToDB();
     console.log(`Server is up and running on port: ${PORT || 4250}`);
 });
